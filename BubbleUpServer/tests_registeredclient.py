b42b9fcb9de50aa509e13eba01a627cb9129905f
Roman Subik <roman.subik@ocado.com>
2016-04-30 16:03:13 +0100

    #7 Tests for RegisteredClient related REST endpoints.

--- BubbleUpServer/tests_registeredclient.py
+++ BubbleUpServer/tests_registeredclient.py
@@ -24,3 +24,38 @@ class AccountTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(RegisteredClient.objects.count(), 1)
 
+    def test_post(self):
+        url = reverse('registered_client')
+        data = {
+            "phrase": "nice wise ox"
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn("country", response.data)
+        self.assertIn("ip", response.data)
+        self.assertIn("date_joined", response.data)
+        self.assertIn("uuid", response.data)
+        self.assertIn("phrase", response.data)
+        self.assertEqual(data["phrase"], response.data["phrase"])
+        self.assertEqual(RegisteredClient.objects.count(), 1)
+
+    def test_post_invalid_phrase(self):
+        url = reverse('registered_client')
+        data = {
+            "phrase": "gibberish text here"
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(RegisteredClient.objects.count(), 0)
+
+    def test_post_to_short_phrase(self):
+        url = reverse('registered_client')
+        data = {
+            "phrase": "nice wise"
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(RegisteredClient.objects.count(), 0)
--- BubbleUpServer/views.py
+++ BubbleUpServer/views.py
@@ -44,7 +44,6 @@ class RegisteredClientList(mixins.ListModelMixin, generics.GenericAPIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
 
 
 class RegisteredClientDetail(generics.RetrieveUpdateDestroyAPIView):
