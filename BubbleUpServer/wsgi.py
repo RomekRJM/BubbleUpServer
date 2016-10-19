352aecc58235406e35d8246c947dee619a2fa198
Roman Subik <romek.rjm@gmail.com>
2016-10-19 21:37:55 +0100

    #19 Fix problems with inability to locate scheduler on EC2.

--- BubbleUpServer/wsgi.py
+++ BubbleUpServer/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 import sys
 
+sys.path.insert(0, '/opt/python/current/app')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BubbleUpServer.settings")
+
 from django.core.wsgi import get_wsgi_application
 from scheduled import scheduler
-
-sys.path.insert(0, '/opt/python/current/app/BubbleUpServer')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BubbleUpServer.settings")
 
 application = get_wsgi_application()
 
